f4623ce41c0e18ee1154bc7b2360899c62392c29
Zũ Ngô <2124801030112@student.tdmu.edu.vn>
2024-09-17 14:59:57 +0700

    v2

--- context/CartContext.js
+++ context/CartContext.js
@@ -1,3 +1,4 @@
+// CartContext.js
 import React, { createContext, useState } from 'react';
 
 // Create CartContext
@@ -7,29 +8,28 @@ export const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);  // Initialize cart as an empty array
 
-  // Function to add item to cart with quantity management
   const addToCart = (item) => {
     setCart((prevCart) => {
+      // Check if item already exists
       const existingItem = prevCart.find((cartItem) => cartItem.idCategory === item.idCategory);
-
       if (existingItem) {
+        // If item exists, increase the quantity
         return prevCart.map((cartItem) =>
           cartItem.idCategory === item.idCategory
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
         );
       } else {
+        // If item doesn't exist, add it to the cart
         return [...prevCart, { ...item, quantity: 1 }];
       }
     });
   };
 
-  // Function to remove an item from cart
   const removeFromCart = (idCategory) => {
     setCart((prevCart) => prevCart.filter((item) => item.idCategory !== idCategory));
   };
 
-  // Function to increase quantity of an item
   const increaseQuantity = (idCategory) => {
     setCart((prevCart) =>
       prevCart.map((item) =>
@@ -38,19 +38,24 @@ export const CartProvider = ({ children }) => {
     );
   };
 
-  // Function to decrease quantity of an item
   const decreaseQuantity = (idCategory) => {
     setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.idCategory === idCategory && item.quantity > 1
-          ? { ...item, quantity: item.quantity - 1 }
-          : item
-      )
+      prevCart
+        .map((item) =>
+          item.idCategory === idCategory && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
     );
   };
 
+  const clearCart = () => {
+    setCart([]);
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart }}>
       {children}
     </CartContext.Provider>
   );
